dbb044fafe76dc3245fd9808f60aea76d70f3310
walterhu <alterhu2020@gmail.com>
2020-03-11 21:17:38 +0800

    doc: update script

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -12,20 +12,6 @@ export default ({
     })
   }
   // Baidu autopsh integration
-  if (process.env.NODE_ENV === "production" && typeof window !== "undefined") {
-    (function() {
-      var bp = document.createElement("script");
-
-      var curProtocol = window.location.protocol.split(":")[0];
-      if (curProtocol === "https") {
-        bp.src = "https://zz.bdstatic.com/linksubmit/push.js";
-      } else {
-        bp.src = "http://push.zhanzhang.baidu.com/push.js";
-      }
-      var s = document.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(bp, s);
-    })();
-  }
   // 配置谷歌广告
   Vue.use(require('vue-script2'))
 
